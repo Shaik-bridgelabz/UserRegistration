c466768891b2fff744aa2acc46194be9b0f4e17b
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-24 17:06:58 +0530

    Add. Adding [UseCase2] to check Last Name of user to branch lastName.

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,9 +5,21 @@ if [[ $fname =~ $pat ]]
 then
 	echo Valid
 else
-	echo Not Valid Reenter Firstname
+	echo Not Valid
+fi
+}
+
+function lastName() {
+pat="^[A-Z]+[a-zA-Z]{2,}$"
+if [[ $lname =~ $pat ]]
+then
+        echo Valid
+else
+        echo Not Valid
 fi
 }
 
 read -p "Enter First Name: " fname
 firstName
+read -p "Enter Last Name: " lname
+lastName
