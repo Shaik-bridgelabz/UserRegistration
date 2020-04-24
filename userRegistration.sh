f2255232e0aed777c4656e1dd09684e799639784
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-24 17:37:14 +0530

    Add. Adding [UseCase7] to check Password Rule3 of user to branch passwordRule3.

--- userRegistration.sh
+++ userRegistration.sh
@@ -43,7 +43,8 @@ fi
 function password(){
 rule1="[a-zA-Z0-9]{8,}"
 rule2="[a-zA-Z0-9][A-Z][a-zA-Z0-9]*"
-if [[ $password =~ $rule1 && $password =~ $rule2 ]]
+rule3="[a-zA-Z0-9]*[0-9][a-zA-Z0-9]*"
+if [[ $password =~ $rule1 && $password =~ $rule2 && $password =~ $rule3 ]]
 then
 	echo Valid
 else
