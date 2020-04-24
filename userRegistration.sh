5f9930ebd45586fe03c48cefc1efdd03ffd06305
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-24 16:53:52 +0530

    Add. Adding [UseCase1] to check First Name of user to branch firstName.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,1 +1,13 @@
 #!/bin/bash -x
+function firstName() {
+pat="^[A-Z]+[a-zA-Z]{2,}$"
+if [[ $fname =~ $pat ]]
+then
+	echo Valid
+else
+	echo Not Valid Reenter Firstname
+fi
+}
+
+read -p "Enter First Name: " fname
+firstName
